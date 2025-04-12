14f6dacddf1e538492c58e3e1c8376b82d10d8e3
MarchLy <hoa1bg1996@gmail.com>
2025-04-12 22:41:18 +0700

    Crud

---
+++ index.php
@@ -0,0 +1,43 @@
+<?php
+
+session_start();
+//Database
+include 'app/Database/Database.php';
+
+//Model
+include 'app/Models/Admin/HomeModel.php';
+include 'app/Models/Admin/UserModel.php';
+include 'app/Models/Admin/CategoryModel.php';
+include 'app/Models/Admin/ProductModel.php';
+include 'app/Models/Admin/commentRatingModel.php';
+include 'app/Models/Admin/OrderModel.php';
+
+include 'app/Models/Users/CategoryUserModel.php';
+include 'app/Models/Users/ProductUserModel.php';
+include 'app/Models/Users/LoginModel.php';
+include 'app/Models/Users/UserModel2.php';
+include 'app/Models/Users/CartUserModel.php';
+include 'app/Models/Users/OrderUserModel.php';
+
+
+
+//Controller
+include 'app/Controllers/Admin/ControllerAdmin.php';
+include 'app/Controllers/Admin/HomeController.php';
+include 'app/Controllers/Admin/LoginController.php';
+include 'app/Controllers/Admin/UserController.php';
+include 'app/Controllers/Admin/CategoryController.php';
+include 'app/Controllers/Admin/ProductController.php';
+include 'app/Controllers/Admin/commentRatingController.php';
+include 'app/Controllers/Admin/OrderController.php';
+
+
+include 'app/Controllers/Users/LoginUserController.php';
+include 'app/Controllers/Users/DashboardController.php';
+
+const BASE_URL = "http://localhost/Agile/" ;
+//Router
+include 'router/web.php';
+// Router test
+
+//echo password_hash("123456", PASSWORD_BCRYPT);
